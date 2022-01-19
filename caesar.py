e66a4ccacedc1de66dca43488fcf28d959ae6576
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-01-18 21:19:50 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -2,10 +2,12 @@
 def encrypt(key,plaintext):
     ciphertext=""
     #YOUR CODE HERE
-    for i in range(len(plaintext)):
-        l = plaintext[i]
-        ciphertext += chr((ord(l) + key - 65) % 26 + 65)
-        
+#     for i in range(len(plaintext)):
+#         l = plaintext[i]
+#         ciphertext += chr((ord(l) + key - 65) % 26 + 65)
+    for l in plaintext:
+#         l = plaintext[i]
+        ciphertext += chr((ord(l) + key - 65) % 26 + 65)        
     return ciphertext
 
 def decrypt(key,ciphertext):
