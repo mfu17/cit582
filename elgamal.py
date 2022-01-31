807f38fc4ef62f57a0085aa69fb9204243067c1e
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-01-31 18:09:43 -0500

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -18,7 +18,7 @@ def keygen():
 #     print(sk)
     pk = pow(g, sk, p)
 #     print(pk)
-    print("keygen")
+#    print("keygen")
     return pk,sk
 
 def encrypt(pk,m):
@@ -30,16 +30,13 @@ def encrypt(pk,m):
     r = random.SystemRandom().randint(1,q)
     c1 = pow(g, r, p)
     c2 = pow(pk, r, p) * (m % p)
-    print("encrypt")
+ #   print("encrypt")
     return [c1,c2]
 
 def decrypt(sk,c):
     m = 0
-#     g=9698439374675276823390863679085787099821687964590973421364530706908301367232976242960049544758496470828675688981570819335336142993170304256564916507021997
-#     p=17485407687035251201370420829093858071027518631263552549047038216080132036645437679594890870680904087373138192057582526597149370808367592630377967178132719
-#     q = (p-1)/2
    # m = (c[1] / pow(c[0], sk)) % p
-    m = (c[1] % p) * pow(c[0], p-1-sk, p)
-    print("decrypt")
+    m = c[1] * pow(c[0], p-1-sk, p)
+#    print("decrypt")
     return m
 
