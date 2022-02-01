ddc4ce53ada48bb0d7a3ebc030d945764a54bf36
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-01-31 19:33:24 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,5 +1,5 @@
 from fastecdsa.curve import secp256k1
-from fastecdsa.keys import export_key, gen_keypair
+from fastecdsa.keys import export_key, gen_keypair, gen_private_key
 
 from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
@@ -13,7 +13,7 @@ def sign(m):
 #	n = G.order()
 	#d = random.SystemRandom().randint(1,n)
 	#fastecdsa.keys.gen_keypair(secp256k1)
-	d = fastecdsa.keys.gen_private_key(secp256k1)
+	d = gen_private_key(secp256k1)
 	public_key = d * secp256k1.G
 
 	#generate signature
@@ -21,7 +21,7 @@ def sign(m):
 	r = 0
 	s = 0
 
-	[r,s] = fastecdsa.ecdsa.sign(m, d, secp256k1, sha256)
+	[r,s] = ecdsa.sign(m, d, secp256k1, sha256)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
