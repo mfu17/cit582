de5aa464c23fe63bfd3dff1770121f0d9226c714
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-03-01 11:48:52 +0800

    Update ethereum_query.py

--- ethereum_query.py
+++ ethereum_query.py
@@ -50,3 +50,20 @@ def get_most_expensive_transaction(block_num):
             max_cost = cur_cost
     #max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
     return max_tx
+
+print(get_transaction_cost('0x0dda1142828634746a8e49e707fddebd487355a172bfa94b906a151062299578')/10**18*1385.2)
+
+blocks = list(range(10237100, 10237110))
+total_cost = 0
+
+for block in blocks:
+    total_cost += get_block_cost(block)/10**18
+print(total_cost)/len(blocks)
+
+block_cost = (get_block_cost(10237208)/10**18+2)*248.26
+print(block_cost)
+
+block_tx = get_most_expensive_transaction(10237208)
+block_tx_cost = get_transaction_cost(block_tx)
+print(block_tx.hex())
+print(block_tx_cost)
