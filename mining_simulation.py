76b73aad43c1748e0ede92af752fdd49ea357b20
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-03-17 09:50:12 +0800

    Update mining_simulation.py

--- mining_simulation.py
+++ mining_simulation.py
@@ -93,7 +93,7 @@ def Simulate(alpha,gamma,N, seed):
   DON'T include it in your final submission though.
 """
 
-#"""
+"""
 #let's run the code with the follwing parameters!
 alpha=0.35
 gamma=0.5
@@ -102,4 +102,4 @@ seed = 100
 #This is the theoretical probability computed in the original paper
 print("Theoretical probability :",(alpha*(1-alpha)**2*(4*alpha+gamma*(1-2*alpha))-alpha**3)/(1-alpha*(1+(2-alpha)*alpha)))
 print("Simulated probability :",Simulate(alpha,gamma,Nsimu, seed))
-#"""
+"""
