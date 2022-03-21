4d827ade2a71553490194f31d68275a9e62cad28
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-03-22 02:22:34 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -10,19 +10,21 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
+    
     content = request.get_json(silent=True)
 
     payload = content.get('payload')
     payload_str = json.dumps(payload)
+    platform = payload.get('platform')
     sig = content.get('sig')
     pk = payload.get('pk')
-    platform = payload.get('platform')
 
-    if platform == 'Algorand':
+    if platform == "Algorand":
         result = algosdk.util.verify_bytes(payload_str.encode('utf-8'), sig, pk)
-    elif platform == 'Ethereum':
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload_str)
-        result = eth_account.Account.recover_message(eth_encoded_msg, signature=sig) == pk
+        
+    elif platform == "Ethereum":
+        eth_encoded_msg = eth_account.messages.encode_defunct(text = payload_str)
+        result = eth_account.Account.recover_message(eth_encoded_msg, signature = sig) == pk
 
     #Check if signature is valid
     #result = True #Should only be true if signature validates
