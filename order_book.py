ff0c4c6da91caee30ab8c9dee4d9f40e54ee8431
mfu17 <90281520+mfu17@users.noreply.github.com>
2022-03-29 00:56:32 +0800

    Add files via upload

---
+++ order_book.py
@@ -0,0 +1,81 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+from models import Base, Order
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def match_order(new_order, old_order):
+    cond1 = new_order.filled == None
+    cond2 = new_order.sell_currency == old_order.buy_currency
+    cond3 = new_order.buy_currency == old_order.sell_currency
+    cond4 = ((new_order.sell_amount * old_order.sell_amount) >= (new_order.buy_amount * old_order.buy_amount))
+    return (cond1 & cond2 & cond3 & cond4) 
+
+def process_order(order):
+    #Your code here
+    buy_currency = order['buy_currency']
+    sell_currency = order['sell_currency']
+    buy_amount = order['buy_amount']
+    sell_amount = order['sell_amount']
+    sender_pk = order['sender_pk']
+    receiver_pk = order['receiver_pk']
+    
+    if order.get('creator_id') == None:
+        new_order = Order(buy_currency = buy_currency,
+                            sell_currency = sell_currency,
+                            buy_amount = buy_amount,
+                            sell_amount = sell_amount,
+                            sender_pk = sender_pk,
+                            receiver_pk = receiver_pk)
+    else: 
+        new_order = Order(buy_currency = buy_currency,
+                            sell_currency = sell_currency,
+                            buy_amount = buy_amount,
+                            sell_amount = sell_amount,
+                            sender_pk = sender_pk,
+                            receiver_pk = receiver_pk,
+                            creator_id = order.get('creator_id'))
+        
+    session.add(new_order)
+    session.commit()
+    
+    unfilled_orders = session.query(Order).filter(Order.filled == None).all()
+    
+    for old_order in unfilled_orders:
+        if match_order(new_order, old_order):
+            
+            old_order.filled = datetime.now()
+            new_order.filled = datetime.now()
+            old_order.counterparty_id = new_order.id
+            new_order.counterparty_id = old_order.id
+            session.commit()
+            
+            if new_order.buy_amount == old_order.sell_amount:
+                return
+            else:
+                child_order = {}
+                if new_order.buy_amount > old_order.sell_amount:
+                    child_order['buy_currency'] = new_order.buy_currency
+                    child_order['sell_currency'] = new_order.sell_currency
+                    child_order['buy_amount'] = new_order.buy_amount - old_order.sell_amount
+                    child_order['sell_amount'] = child_order['buy_amount']*(new_order.sell_amount/new_order.buy_amount)*1.01
+                    child_order['sender_pk'] = new_order.sender_pk
+                    child_order['receiver_pk'] = new_order.receiver_pk
+                    child_order['creator_id'] = new_order.id
+                    
+                elif new_order.buy_amount < old_order.sell_amount:
+                    child_order['buy_currency'] = old_order.buy_currency
+                    child_order['sell_currency'] = old_order.sell_currency
+                    child_order['sell_amount'] = old_order.sell_amount - new_order.buy_amount
+                    child_order['buy_amount'] = child_order['sell_amount']*(old_order.buy_amount/old_order.sell_amount)*0.99
+                    child_order['sender_pk'] = old_order.sender_pk
+                    child_order['receiver_pk'] = old_order.receiver_pk
+                    child_order['creator_id'] = old_order.id
+                    
+                process_order(child_order)
+    
+    #pass
